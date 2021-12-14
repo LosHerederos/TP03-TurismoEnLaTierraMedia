98d3bc6bac0a625ec2a1e7238cab68ffeafcf939
Pedro Rolando <pmrolando@gmail.com>
2021-12-14 16:35:28 -0300

    Modificaciones en metodo insert de PromocionDAOImpl

--- src/main/java/persistence/impl/PromocionDAOImpl.java
+++ src/main/java/persistence/impl/PromocionDAOImpl.java
@@ -99,8 +99,8 @@ public class PromocionDAOImpl implements PromocionDAO {
 	public int insert(Promocion promocion) {
 		try {
 			String sql = "INSERT INTO Promociones (\n"
-					+ "nombre, descripcion, imagen, idTipoDeAtraccion, tipoDePromocion\n"
-					+ ") VALUES (?, ? ,?, ?, ?)";
+				+ "nombre, descripcion, imagen, idTipoDeAtraccion, tipoDePromocion\n"
+				+ ") VALUES (?, ? ,?, ?, ?)";
 			
 			Connection conexion = ConnectionProvider.getConnection();
 			PreparedStatement statement = conexion.prepareStatement(sql);
@@ -113,6 +113,30 @@ public class PromocionDAOImpl implements PromocionDAO {
 			promocion.setIdPromocion(buscarIdUltimaPromocion());
 			insertAtracciones(promocion.getIdPromocion(), promocion.getAtracciones(), false);
 			
+			String tipoDePromocion = promocion.getClass().getSimpleName();
+			if (tipoDePromocion.equals("PromocionAbsoluta")) {
+				sql = "INSERT INTO PromocionAbsoluta (\n"
+					+ "idPromocion, costoTotal\n"
+					+ ") VALUES (?, ?)";
+				int costoTotal = ((PromocionAbsoluta) promocion).getCostoTotal();
+				statement.close();
+				statement = conexion.prepareStatement(sql);
+				statement.setInt(1, promocion.getIdPromocion());
+				statement.setInt(2, costoTotal);
+				filas = statement.executeUpdate();
+			} else if (tipoDePromocion.equals("PromocionAXB")) {
+				insertAtracciones(promocion.getIdPromocion(), ((PromocionAXB) promocion).getAtraccionesPagas(), true);
+			} else if (tipoDePromocion.equals("PromocionPorcentual")) {
+				sql = "INSERT INTO PromocionPorcentual (\n"
+					+ "idPromocion, porcentaje\n"
+					+ ") VALUES (?, ?)";
+				double porcentaje = ((PromocionPorcentual) promocion).getPorcentaje();
+				statement.close();
+				statement = conexion.prepareStatement(sql);
+				statement.setInt(1, promocion.getIdPromocion());
+				statement.setDouble(2, porcentaje);
+				filas = statement.executeUpdate();
+			}
 			return filas;
 		}  catch (Exception e) {
 			throw new MissingDataException(e);
@@ -163,11 +187,11 @@ public class PromocionDAOImpl implements PromocionDAO {
 	public int update(Promocion promocion) {
 		try {
 			String sql = "UPDATE Promociones\n"
-					+ "SET nombre = ?,\n"
-					+ "descripcion = ?,\n"
-					+ "imagen = ?,\n"
-					+ "idTipoDeAtraccion = ?\n"
-					+ "WHERE idPromocion = ?;";
+				+ "SET nombre = ?,\n"
+				+ "descripcion = ?,\n"
+				+ "imagen = ?,\n"
+				+ "idTipoDeAtraccion = ?\n"
+				+ "WHERE idPromocion = ?;";
 			Connection conexion = ConnectionProvider.getConnection();
 			PreparedStatement statement = conexion.prepareStatement(sql);
 			statement.setString(1, promocion.getNombre());
@@ -176,6 +200,31 @@ public class PromocionDAOImpl implements PromocionDAO {
 			statement.setInt(4, promocion.getTipoDeAtraccion().ordinal()+1);
 			statement.setInt(5, promocion.getIdPromocion());
 			int filas = statement.executeUpdate();
+			
+			String tipoDePromocion = promocion.getClass().getSimpleName();
+			if (tipoDePromocion.equals("PromocionAbsoluta")) {
+				sql = "UPDATE PromocionAbsoluta\n"
+					+ "SET costoTotal = ?\n"
+					+ "WHERE idPromocion = ?;";
+				int costoTotal = ((PromocionAbsoluta) promocion).getCostoTotal();
+				statement.close();
+				statement = conexion.prepareStatement(sql);
+				statement.setInt(1, costoTotal);
+				statement.setInt(2, promocion.getIdPromocion());
+				filas = statement.executeUpdate();
+			} else if (tipoDePromocion.equals("PromocionAXB")) {
+//				insertAtracciones(promocion.getIdPromocion(), ((PromocionAXB) promocion).getAtraccionesPagas(), true);
+			} else if (tipoDePromocion.equals("PromocionPorcentual")) {
+				sql = "UPDATE PromocionPorcentual\n"
+					+ "SET porcentaje = ?\n"
+					+ "WHERE idPromocion = ?;";
+				double porcentaje = ((PromocionPorcentual) promocion).getPorcentaje();
+				statement.close();
+				statement = conexion.prepareStatement(sql);
+				statement.setDouble(1, porcentaje);
+				statement.setInt(2, promocion.getIdPromocion());
+				filas = statement.executeUpdate();
+			}
 			return filas;
 		}  catch (Exception e) {
 			throw new MissingDataException(e);
--- src/main/webapp/admin/promociones/promocion.jsp
+++ src/main/webapp/admin/promociones/promocion.jsp
@@ -15,29 +15,75 @@
 	<jsp:include page="/partials/nav.jsp"></jsp:include>
 	<main class="container">
 		<h1>Promoción</h1>
-		<img width="300" src="<c:url value="/assets/img/uploads/promociones/${promocionAVer.imagen}"></c:url>">
-		<p>Id: <c:out value="${promocionAVer.idPromocion}"></c:out></p>
-		<p>Nombre: <c:out value="${promocionAVer.nombre}"></c:out></p>
-		<p>Tipo de promoción: <c:out value="${promocionAVer.getClass().getSimpleName()}"></c:out></p>
-		<p>Tipo de atracción: <c:out value="${promocionAVer.tipoDeAtraccion}"></c:out></p>
-		<p>Descripción: <c:out value="${promocionAVer.descripcion}"></c:out></p>
-		<p>Costo: <c:out value="${promocionAVer.getCosto()}"></c:out></p>
-		<p>Tiempo: <c:out value="${promocionAVer.getTiempo()}"></c:out></p>
-		<div>
-			<p>Atracciones:</p>
-			<ul>
-				<c:forEach items="${promocionAVer.atracciones}" var="atraccion">
-					<li><c:out value="${atraccion}"></c:out></li>
-				</c:forEach>
-			</ul>
-			<c:if test="${promocionAVer.getClass().getSimpleName().equals('PromocionAXB')}">
-				<p>Atracciones pagas:</p>
-				<ul>
-					<c:forEach items="${promocionAVer.atraccionesPagas}" var="atraccionPaga">
-						<li><c:out value="${atraccionPaga}"></c:out></li>
-					</c:forEach>
-				</ul>
-			</c:if>
+		<div class="row">
+			<div class="col-9">
+				<p>Id: <c:out value="${promocionAVer.idPromocion}"></c:out></p>
+				<p>Nombre: <c:out value="${promocionAVer.nombre}"></c:out></p>
+				<p>Tipo de promoción: <c:out value="${promocionAVer.getClass().getSimpleName()}"></c:out></p>
+				<p>Tipo de atracción: <c:out value="${promocionAVer.tipoDeAtraccion}"></c:out></p>
+				<p>Descripción: <c:out value="${promocionAVer.descripcion}"></c:out></p>
+				<p>Costo: <c:out value="${promocionAVer.getCosto()}"></c:out></p>
+				<p>Tiempo: <c:out value="${promocionAVer.getTiempo()}"></c:out></p>
+			</div>
+			<div class="col-3">
+				<img style="width: 100%" src="<c:url value="/assets/img/uploads/promociones/${promocionAVer.imagen}"></c:url>">
+			</div>
+		</div>		
+		<div class="row">
+			<div class="col-12">
+				<p>Atracciones:</p>
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Nombre</th>
+							<th>Tipo de atracción</th>
+							<th>Costo</th>
+							<th>Tiempo</th>
+							<th>Cupo</th>
+							<th>Cupo ocupado</th>
+						</tr>
+					</thead>
+					<tbody>
+						<c:forEach items="${promocionAVer.atracciones}" var="atraccion">
+							<tr>
+								<td><c:out value="${atraccion.nombre}"></c:out></td>
+								<td><c:out value="${atraccion.tipoDeAtraccion}"></c:out></td>
+								<td><c:out value="${atraccion.costo}"></c:out></td>
+								<td><c:out value="${atraccion.tiempo}"></c:out></td>
+								<td><c:out value="${atraccion.cupoPersonas}"></c:out></td>
+								<td><c:out value="${atraccion.visitantes}"></c:out></td>
+							</tr>
+						</c:forEach>
+					</tbody>
+				</table>
+				<c:if test="${promocionAVer.getClass().getSimpleName().equals('PromocionAXB')}">
+					<p>Atracciones pagas:</p>
+					<table class="table">
+						<thead>
+							<tr>
+								<th>Nombre</th>
+								<th>Tipo de atracción</th>
+								<th>Costo</th>
+								<th>Tiempo</th>
+								<th>Cupo</th>
+								<th>Cupo ocupado</th>
+							</tr>
+						</thead>
+						<tbody>
+							<c:forEach items="${promocionAVer.atraccionesPagas}" var="atraccionPaga">
+								<tr>
+									<td><c:out value="${atraccionPaga.nombre}"></c:out></td>
+									<td><c:out value="${atraccionPaga.tipoDeAtraccion}"></c:out></td>
+									<td><c:out value="${atraccionPaga.costo}"></c:out></td>
+									<td><c:out value="${atraccionPaga.tiempo}"></c:out></td>
+									<td><c:out value="${atraccionPaga.cupoPersonas}"></c:out></td>
+									<td><c:out value="${atraccionPaga.visitantes}"></c:out></td>
+								</tr>
+							</c:forEach>
+						</tbody>
+					</table>
+				</c:if>
+			</div>
 		</div>
 	</main>
 </body>
